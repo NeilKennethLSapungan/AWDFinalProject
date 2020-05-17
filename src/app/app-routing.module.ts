c6c8f06624c4df1f9846f190519f07dfc517c61d
Tenzionado <you@example.com2047584@aup.edu.ph>
2020-05-17 08:55:48 +0800

    add a router to route the login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
+import { HomeComponent } from './home/home.component';
 import { RegisterSignInComponent } from './register-sign-in/register-sign-in.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: RegisterSignInComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
   {
     path : '',
     redirectTo: '/home',
