fb5c0490cb0489277d777be47e34b7a2b15c1239
Tenzionado <you@example.com2047584@aup.edu.ph>
2020-05-17 08:39:18 +0800

    add login page to our component section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { RegisterSignInComponent } from './register-sign-in/register-sign-in.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
